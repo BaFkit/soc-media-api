e28425f4b7d7addda1615c6f83be71f1529cf72a
bafkit <konyrevpv@mail.ru>
2023-08-20 17:55:46 +0300

    Working with a project: Customization, refinement and documentation

--- src/main/java/com/example/socialmediaapi/dto/requests/LoginRequest.java
+++ src/main/java/com/example/socialmediaapi/dto/requests/LoginRequest.java
@@ -1,11 +1,14 @@
 package com.example.socialmediaapi.dto.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
+    @Schema(title = "Login", description = "Username or email", example = "user1", requiredMode = Schema.RequiredMode.REQUIRED)
     private String login;
+    @Schema(title = "Password", description = "Password", example = "100", requiredMode = Schema.RequiredMode.REQUIRED)
     private String password;
 
 }
--- src/main/java/com/example/socialmediaapi/dto/requests/MessageDtoIn.java
+++ src/main/java/com/example/socialmediaapi/dto/requests/MessageDtoIn.java
@@ -1,5 +1,6 @@
 package com.example.socialmediaapi.dto.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.UUID;
@@ -7,7 +8,9 @@ import java.util.UUID;
 @Data
 public class MessageDtoIn {
 
+    @Schema(title = "Id", description = "Message recipient Id", example = "5", requiredMode = Schema.RequiredMode.REQUIRED)
     private UUID addresseeId;
+    @Schema(title = "Message content", description = "Any text", example = "Just Message", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String text;
 
 }
